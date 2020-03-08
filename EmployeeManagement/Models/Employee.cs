6ae1a6f01a0a21bca74433754f78867230520995
Jordan J. Naeem <naeem.2@buckeyemail.osu.edu>
2020-03-08 16:44:56 -0500

    Pragim Tech Ep. 42 -- Model Validation

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace EmployeeManagement.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name { get; set;}
+        [Required]
+        [RegularExpression(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z",
+            ErrorMessage ="Invalid email format")]
+        [Display(Name = "Office Email")]
         public string Email { get; set; }
         public Dept Department { get; set; }
     }
