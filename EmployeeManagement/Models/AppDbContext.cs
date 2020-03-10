8e1c6b2b6fad04b2201d9a0efbf5f7d306bb9ce2
JJNeemz <32807514+JJNeemz@users.noreply.github.com>
2020-03-09 19:14:19 -0500

    Pragim Tech Ep. 48 -- Using sql server with entity framework core

---
+++ EmployeeManagement/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class AppDbContext : DbContext
+    {
+        //Create constructor for DbContext options used for configuration information 
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        //This property is used to query and saves instances of the Employee class
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -6,6 +6,7 @@ using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,10 +27,14 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+
             services.AddMvc(options => options.EnableEndpointRouting = false).AddXmlSerializerFormatters();
             //The following line is saying, when someone requests the IEmployeeRepository interface,
             //Create an instance of the MockEmployeeRepository class and inject that instance
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
