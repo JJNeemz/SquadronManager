36a396614debc86bc623cc9be2e6e438d67d9b1b
JJNeemz <32807514+JJNeemz@users.noreply.github.com>
2020-03-16 14:35:31 -0500

    Pragim Tech Ep. 67 -- Identity UserManager and SignInManager

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,13 +12,51 @@ namespace EmployeeManagement.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
 
-        
+        [HttpPost]
+        // Need to make method async because of CreateAsync method
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+                // Must use await keyword due to CreateAsync asynchronous method
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                // Log in user if CreateAsync succeeds
+                if (result.Succeeded)
+                {
+                    // Set isPersistent to false -- we don't want a permanent cookie
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+                // If CreateAsync fails, add the errors to the modelstate to be displayed on the validation summary tag helper
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            
+            return View(model);
+        }
 
     }
 }
