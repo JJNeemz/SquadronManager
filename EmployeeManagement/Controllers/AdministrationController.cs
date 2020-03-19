c3102d00261f5b7af5d80e70b5a57825f915d858
JJNeemz <32807514+JJNeemz@users.noreply.github.com>
2020-03-19 09:44:46 -0500

    Pragim Tech Ep. 82 -- Role Based Authorization

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace EmployeeManagement.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Admin")]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
