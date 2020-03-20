7a71b336a595c87be200a329750d276b4a9e477a
JJNeemz <32807514+JJNeemz@users.noreply.github.com>
2020-03-20 13:52:36 -0500

    Pragim Tech Ep. 87 -- Delete Confirmation

---
+++ EmployeeManagement/wwwroot/js/CustomScript.js
@@ -0,0 +1,12 @@
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+    var deleteSpan = `deleteSpan_${uniqueId}`;
+    var confirmDeleteDiv = `confirmDeleteDiv_${uniqueId}`;
+
+    if (isDeleteClicked) {
+        $(`#${deleteSpan}`).hide();
+        $(`#${confirmDeleteDiv}`).show();
+    } else {
+        $(`#${deleteSpan}`).show();
+        $(`#${confirmDeleteDiv}`).hide();
+    }
+}
