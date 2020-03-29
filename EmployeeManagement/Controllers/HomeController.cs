40fecc926cd09775970c1e7fea065d101d8f135e
JJNeemz <32807514+JJNeemz@users.noreply.github.com>
2020-03-29 16:08:36 -0500

    Added modal delete user confirmation in ListRoles view

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using EmployeeManagement.Models;
-using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         public ViewResult Index()
