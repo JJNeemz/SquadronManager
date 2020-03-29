ad0a8705d6ad1d49e14b674bea158924ea38b7ea
JJNeemz <32807514+JJNeemz@users.noreply.github.com>
2020-03-29 16:21:51 -0500

    Added delete confirmation modal to ListRoles view in Administration

--- EmployeeManagement/wwwroot/js/CustomScript.js
+++ EmployeeManagement/wwwroot/js/CustomScript.js
@@ -1,12 +1,1 @@
-﻿function confirmDelete(uniqueId, isDeleteClicked) {
-    var deleteSpan = `deleteSpan_${uniqueId}`;
-    var confirmDeleteDiv = `confirmDeleteDiv_${uniqueId}`;
-
-    if (isDeleteClicked) {
-        $(`#${deleteSpan}`).hide();
-        $(`#${confirmDeleteDiv}`).show();
-    } else {
-        $(`#${deleteSpan}`).show();
-        $(`#${confirmDeleteDiv}`).hide();
-    }
-}
+﻿
